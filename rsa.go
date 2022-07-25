02cad0ad331ad58ec7082696074b5b4dd715a3ee
seph <seph@kolide.co>
2022-07-24 21:19:58 -0400

    primatives

---
+++ rsa.go
@@ -0,0 +1,42 @@
+package krypto
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/sha256"
+	"fmt"
+)
+
+func rsaEncrypt(key *rsa.PublicKey, secretMessage []byte) ([]byte, error) {
+	return rsa.EncryptOAEP(sha1.New(), rand.Reader, key, secretMessage, nil)
+}
+
+func rsaDecrypt(key *rsa.PrivateKey, ciphertext []byte) ([]byte, error) {
+	return rsa.DecryptOAEP(sha1.New(), rand.Reader, key, ciphertext, nil)
+}
+
+func rsaSign(key *rsa.PrivateKey, message []byte) ([]byte, error) {
+	hasher := sha256.New()
+	if _, err := hasher.Write(message); err != nil {
+		return nil, fmt.Errorf("hashing message: %w", err)
+	}
+	digest := hasher.Sum(nil)
+
+	return rsa.SignPSS(rand.Reader, key, crypto.SHA256, digest, nil)
+}
+
+func rsaVerify(key *rsa.PublicKey, message []byte, sig []byte) error {
+	hasher := sha256.New()
+	if _, err := hasher.Write(message); err != nil {
+		return fmt.Errorf("hashing message: %w", err)
+	}
+	digest := hasher.Sum(nil)
+
+	return rsa.VerifyPSS(key, crypto.SHA256, digest, sig, nil)
+}
+
+func rsaRandomKey() ( *rsa.PrivateKey, error {
+		return rsa.GenerateKey(rand.Reader, 2048)
+}
