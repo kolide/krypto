e692e661eda31734a1be68dee07d954bc29ce59f
James Pickett <jamesandrewpickett@gmail.com>
2025-02-26 14:28:53 -0800

    remove unneeded temp key creation

--- pkg/secureenclave/secureenclave.go
+++ pkg/secureenclave/secureenclave.go
@@ -82,8 +82,10 @@ func (s *SecureEnclaveSigner) Sign(rand io.Reader, digest []byte, opts crypto.Si
 }
 
 // CreateKey creates a new secure enclave key and returns the public key.
-func CreateKey() (*ecdsa.PublicKey, error) {
-	wrapper := C.wrapCreateKey()
+// Passing false for isPermenant will create a temporary key and provides
+// a way to tell if the secure enclave is actually available
+func CreateKey(isPermenant bool) (*ecdsa.PublicKey, error) {
+	wrapper := C.wrapCreateKey(C.bool(isPermenant))
 	result, err := unwrap(wrapper)
 	if err != nil {
 		return nil, err
--- pkg/secureenclave/secureenclave.m
+++ pkg/secureenclave/secureenclave.m
@@ -67,7 +67,7 @@ CFDataRef ExtractPubKey(SecKeyRef pubKey) {
   return res;
 }
 
-size_t createKey(unsigned char** ret, char** retErr){
+size_t createKey(unsigned char** ret, char** retErr, bool isPermanent){
     CFErrorRef error = NULL;
 
     // ignoring the depreciated warning for "kSecAttrAccessibleAlwaysThisDeviceOnly" becuase this is the setting
@@ -95,7 +95,7 @@ size_t createKey(unsigned char** ret, char** retErr){
         (id)kSecAttrKeySizeInBits: @256,
         (id)kSecAttrTokenID: (id)kSecAttrTokenIDSecureEnclave,
         (id)kSecPrivateKeyAttrs:
-        @{  (id)kSecAttrIsPermanent: @YES,
+        @{  (id)kSecAttrIsPermanent: isPermanent ? @YES : @NO,
             (id)kSecAttrAccessControl: (id)access,
         },
     };
@@ -262,12 +262,12 @@ size_t sign(unsigned char* hash, unsigned char* data, int dataSize, unsigned cha
     return size;
 }
 
-Wrapper *wrapCreateKey() {
+Wrapper *wrapCreateKey(bool isPermanent) {
 	Wrapper *res = (Wrapper *)malloc(sizeof(Wrapper));
 	if (!res)
 		return NULL;
 	memset(res, 0, sizeof(Wrapper));
-	res->size = createKey(&res->buf, &res->error);
+	res->size = createKey(&res->buf, &res->error, isPermanent);
 	return res;
 }
 
